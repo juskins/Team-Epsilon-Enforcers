e9a291d3eea335550cb0a804dd4cc44b6460e2a4
FeleAyodeji <feledeji@gmail.com>
2026-03-24 23:42:57 +0100

    feat: add backend setup

---
+++ wastewatch-backend/wastewatch-backend/wastewatch-backend/src/main/java/com/wastewatch/WastewatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wastewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastewatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastewatchBackendApplication.class, args);
+	}
+
+}
